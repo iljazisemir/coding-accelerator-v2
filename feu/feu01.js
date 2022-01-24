7b4091dd95464c442d901ef511204036321681c4
Semir Iljazi <iljazi.semir22@gmail.com>
2022-01-24 19:53:22 +0100

    Ajout fonctions feu00

--- feu/feu01.js
+++ feu/feu01.js
@@ -10,14 +10,19 @@ const removeSpace = (argv) => {
   }
   return argvSplit;
 };
+const argvSplit = removeSpace(argv);
 
 // Chercher les parenthèses
 const findBracket = (argv) => {
   let insideBracket = [];
+  let indexFirstBracket = "";
+  let indexLastBracket = "";
   for (i = 0; i < argv.length; i++) {
     if (argv[i] == "(") {
+      indexFirstBracket = i;
       for (j = i + 1; j < argv.length; j++) {
         if (argv[j] == ")") {
+          indexLastBracket = j;
           break;
         } else {
           insideBracket.push(argv[j]);
@@ -25,7 +30,12 @@ const findBracket = (argv) => {
       }
     }
   }
-  return insideBracket;
+  return pushResultInArray(
+    insideBracket,
+    indexFirstBracket,
+    indexLastBracket,
+    argv
+  );
 };
 
 // Trouver un opérateur spécifique en paramètre
@@ -36,13 +46,13 @@ const findOperator = (argv, operator) => {
     if (argv[i] == operator) {
       let numbersAfterOperator = "";
       for (j = i + 1; j < argv.length; j++) {
-        if (!isNaN(+argv[j])) numbersAfterOperator += argv[j];
-        if (!isNaN(+argv[j]) == false) break;
+        if (!isNaN(+argv[j])) {
+          numbersAfterOperator += argv[j];
+        } else {
+          break;
+        }
       }
-      
-      console.log(
-        calcul(+numberBeforeOperator, argv[i], +numbersAfterOperator)
-      );
+      return calcul(+numberBeforeOperator, operator, +numbersAfterOperator);
     }
     if (!isNaN(+argv[i]) == false) {
       numberBeforeOperator = "";
@@ -60,12 +70,72 @@ const calcul = (n1, operator, n2) => {
   if (operator == "-") return n1 - n2;
 };
 
+// Trier les opérateurs dans l'ordre de priorité
+const sortOperator = (array) => {
+  let arrayOfOperator = [];
+  for (i = 0; i < array.length; i++) {
+    if (array[i] == "%") {
+      arrayOfOperator.push(array[i]);
+    }
+  }
+  for (i = 0; i < array.length; i++) {
+    if (array[i] == "/" || array[i] == "*") {
+      arrayOfOperator.push(array[i]);
+    }
+  }
+  for (i = 0; i < array.length; i++) {
+    if (array[i] == "-" || array[i] == "+") {
+      arrayOfOperator.push(array[i]);
+    }
+  }
+  return arrayOfOperator;
+};
+
+// Return l'argument avec le résultat de l'opération dans le tableau
+const pushResultInArray = (array, firtIndex, lastIndex, argv) => {
+  let argvWithoutIndex = [];
+  let totalInsideArgv = 0;
+  const arraySortOfOperator = sortOperator(array);
+  for (i = 0; i < firtIndex; i++) {
+    argvWithoutIndex.push(argv[i]);
+  }
+  for (k = 0; k < arraySortOfOperator.length; k++) {
+    totalInsideArgv += findOperator(array, arraySortOfOperator[k]);
+  }
+  argvWithoutIndex.push(totalInsideArgv);
+  for (j = lastIndex + 1; j < argv.length; j++) {
+    argvWithoutIndex.push(argv[j]);
+  }
+  return argvWithoutIndex;
+};
+
 // Gestion des erreurs
 
 // Parsing
-const argvSplit = removeSpace(argv);
-
+const argvWithoutBracket = findBracket(argvSplit);
+let result = 0;
+let newArgv = [];
 // Résolution
 
 // Résultat
-console.log(findOperator(findBracket(argvSplit), "/"));
+const newArraySortOfOperator = sortOperator(argvWithoutBracket);
+let indexNumberBeforeOperator = "";
+for (a = 0; a < newArraySortOfOperator.length; a++) {
+  for (b = 0; b < argvWithoutBracket.length; b++) {
+    if (argvWithoutBracket[b] == newArraySortOfOperator[a]) {
+      for (c = b; c > 0; c--) {
+        if (!isNaN(+argvWithoutBracket[c]) == false) {
+          indexNumberBeforeOperator = c + 1;
+        }
+      }
+      let indexNmberAfterOperator = "";
+      for (d = b; d < argvWithoutBracket.length; d++) {
+        if (!isNaN(+argvWithoutBracket[d]) == false) {
+          indexNmberAfterOperator = d - 1;
+        }
+      }
+    }
+  }
+  result += findOperator(argvWithoutBracket, newArraySortOfOperator[a]);
+}
+console.log(result);
